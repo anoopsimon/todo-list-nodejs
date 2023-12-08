1f851231541f78f21a0ee480933b7ef0adaa6fb3
Anoop Simon <Anoop.Simon@health.vic.gov.au>
2023-12-08 15:02:18 +1100

    added styling to ui added a toaster on post add, delete actions

--- public/app.js
+++ public/app.js
@@ -17,6 +17,7 @@ document.getElementById('newTodoForm').addEventListener('submit', function(e) {
   .then(data => {
     addTodoToUI(data); // Update the UI with the new todo
     console.log('Success:', data);
+    showToast('Todo added successfully');
   })
   .catch((error) => {
     console.error('Error:', error);
@@ -27,6 +28,8 @@ document.getElementById('newTodoForm').addEventListener('submit', function(e) {
 function addTodoToUI(todo) {
   const todoList = document.getElementById('todoList');
   const row = todoList.insertRow();
+  row.id = `todo-${todo._id}`; // Set the ID of the row
+
 
   const nameCell = row.insertCell(0);
   nameCell.textContent = todo.name;
@@ -41,8 +44,26 @@ function addTodoToUI(todo) {
   labelCell.textContent = todo.label;
 
   const actionsCell = row.insertCell(4);
-  actionsCell.innerHTML = `<button onclick="editTodo('${todo._id}')">Edit</button>
-                           <button onclick="deleteTodo('${todo._id}')">Delete</button>`;
+    actionsCell.innerHTML = `<button onclick="editTodo('${todo._id}')">Edit</button>
+                             <button onclick="deleteTodo('${todo._id}', '${row.id}')">Delete</button>`;
+}
+
+
+function deleteTodo(todoId, rowId) {
+  fetch(`/todos/${todoId}`, {
+      method: 'DELETE'
+  })
+  .then(response => response.json())
+  .then(data => {
+      // Remove the todo item from the UI
+      const rowToRemove = document.getElementById(rowId);
+      if (rowToRemove) {
+          rowToRemove.remove();
+      }
+      console.log('Todo deleted:', data);
+      showToast('Todo deleted successfully');
+  })
+  .catch(error => console.error('Error:', error));
 }
 
 
@@ -56,6 +77,21 @@ function loadAndDisplayTodos() {
   })
   .catch(error => console.error('Error loading todos:', error));
 }
+
+function showToast(message) {
+  const toastContainer = document.getElementById('toastContainer');
+  const toast = document.createElement('div');
+  toast.className = 'toast';
+  toast.textContent = message;
+
+  toastContainer.appendChild(toast);
+
+  // Remove the toast after the animation
+  setTimeout(() => {
+      toastContainer.removeChild(toast);
+  }, 3000); // 3 seconds for fade out
+}
+
 
 // Call this function when the page loads
 loadAndDisplayTodos();
